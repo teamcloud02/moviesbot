53757e096f8ddc1a974d0be90580e50a7ba34c7c
teamcloud02 <107564042+teamcloud02@users.noreply.github.com>
2022-06-29 12:11:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/TEAMWORKBOTS')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/teamcloud2')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -44,8 +44,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('➡️Channel', url='https://t.me/TEAMWORKBOTS'),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TEAMWORKBOTS')
+            InlineKeyboardButton('➡️Channel', url='https://t.me/teamcloud2'),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/teamcloud2')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TEAMWORKBOTS')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/teamcloud2')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
