166f96c5baa406e0ee71bca4aa082b62259563f0
teamcloud02 <107564042+teamcloud02@users.noreply.github.com>
2022-07-05 20:35:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase If You Want This Movie Request Us. Request Format - https://t.me/teamcloud2/100)
+            k = await query.message.edit('This Movie Not Found In DataBase If You Want This Movie Request Us. Request Format - https://t.me/teamcloud2/100')
             await asyncio.sleep(10)
             await k.delete()
 
