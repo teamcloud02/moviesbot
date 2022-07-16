ae244b264fe6b20eeeaba58694be75131cc66da0
teamcloud02 <107564042+teamcloud02@users.noreply.github.com>
2022-07-16 22:04:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -680,7 +680,7 @@ async def auto_filter(client, msg, spoll=False):
         )
 
     btn.append([
-                InlineKeyboardButton("🤔 How To Download ", url=f"https://t.me/teamcloud2/117"),
+                InlineKeyboardButton("🤔 How To Download ", url=f"https://t.me/teamcloud2/126"),
             ])
             
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
